3aed22d23136536cb766170932e20b6e76fb4bf8
Sunwook Lee <developsun04@gmail.com>
2025-06-14 12:51:16 +0900

    Update List & Tuple.py

--- List & Tuple.py
+++ List & Tuple.py
@@ -1,11 +1,11 @@
 # List와 Tuple - Python 자료구조
-#list와 tuple은 파이썬에서 데이터를 저장하고 관리하는데 사용 되는 가장 기본적인 자료입니다
+# List와 Tuple은 파이썬에서 데이터를 저장하고 관리하는데 사용 되는 가장 기본적인 자료입니다
 
 # 1. index (인덱스)
-#각 항목에 부여된 고유한 번호라고 생각하면 된다(두가지의 세는 방법이 있는대 하나는 (positive)0부터세는 섯이고, 두번째는 뒤에서부터 (negative)1부터 세는 것이다)
+# 각 항목에 부여된 고유한 번호라고 생각하면 된다(두가지의 세는 방법이 있는대 하나는 (positive)0부터세는 섯이고, 두번째는 뒤에서부터 (negative)1부터 세는 것이다)
 
 # 2. List (리스트)
-#리스트는 여러 개의 항목을 순서대로 저장할 수 있는 변경 가능한 (mutable) 데이터 타입입니다.
+# 리스트는 여러 개의 항목을 순서대로 저장할 수 있는 변경 가능한 (mutable) 데이터 타입입니다.
 game=['overwatch','roblox','battlegrounds']
 
 # 2-1. 항목 추가하기 (append())
@@ -18,12 +18,12 @@ game.insert(2,"chess")
 game.remove("chess")
             
 # 2-4. 인덱스로 항목 제거하기 (pop())
-game2=game.pop(0)            
+game.pop(0)            
             
 # 2-5. 항목 찾기 (index())
-game3=game.index("battlegrounds")
+game.index("battlegrounds")
 
 # 2-6. 리스트 슬라이싱
           
 # 3. Tuple (튜플)
-#튜플은 리스트와 비슷하지만, 값이 변경되지 않는 불변 데이터 타입입니다(변경 불가능한(immutable)
+#튜플은 리스트와 비슷하지만, 값이 변경되지 않는 변경 불가능한(immutable) 데이터 타입입니다.
